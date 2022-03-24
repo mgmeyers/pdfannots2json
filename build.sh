18488ea61a82fbdeb3b19bf060009da6f838ce5d
mgmeyers <matthew.gregory.meyers@gmail.com>
2022-03-23 19:27:27 -0700

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# https://words.filippo.io/easy-windows-and-linux-cross-compilers-for-macos/
 
 echo "=== Building mac amd64 ==="
 CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o ./dist/pdf-annots2json.darwin.amd64 .
@@ -7,8 +6,10 @@ CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o ./dist/pdf-annots2json.darwin
 echo "=== Building mac arm64 ==="
 CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -o ./dist/pdf-annots2json.darwin.arm64 .
 
+# https://github.com/messense/homebrew-macos-cross-toolchains
 echo "=== Building linux amd64 ==="
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC="x86_64-unknown-linux-gnu-gcc" go build -o ./dist/pdf-annots2json.linux.amd64 .
 
+# https://words.filippo.io/easy-windows-and-linux-cross-compilers-for-macos/
 echo "=== Building windows amd64 ==="
 CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC="x86_64-w64-mingw32-gcc" go build -o ./dist/pdf-annots2json.windows.amd64.exe .
