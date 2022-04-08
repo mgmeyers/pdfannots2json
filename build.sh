47f0bb0e3f2614266d11dcc01349fad4ccc11941
mgmeyers <matthew.gregory.meyers@gmail.com>
2022-04-08 09:22:08 -0700

    Update dist names

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ mkdir ./dist
 echo "=== Building mac amd64 ==="
 CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -trimpath -o ./dist/pdf-annots2json .
 cd ./dist
-tar -czvf ./pdf-annots2json.darwin.amd64.tar.gz ./pdf-annots2json
+tar -czvf ./pdf-annots2json.mac.intel.tar.gz ./pdf-annots2json
 rm ./pdf-annots2json
 cd ..
 
 echo "=== Building mac arm64 ==="
 CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -trimpath -o ./dist/pdf-annots2json .
 cd ./dist
-tar -czvf ./pdf-annots2json.darwin.arm64.tar.gz ./pdf-annots2json
+tar -czvf ./pdf-annots2json.mac.m1.tar.gz ./pdf-annots2json
 rm ./pdf-annots2json
 cd ..
 
@@ -21,7 +21,7 @@ cd ..
 echo "=== Building linux amd64 ==="
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC="x86_64-unknown-linux-gnu-gcc" go build -trimpath -o ./dist/pdf-annots2json .
 cd ./dist
-tar -czvf ./pdf-annots2json.linux.amd64.tar.gz ./pdf-annots2json
+tar -czvf ./pdf-annots2json.linux.x64.tar.gz ./pdf-annots2json
 rm ./pdf-annots2json
 cd ..
 
@@ -29,6 +29,6 @@ cd ..
 echo "=== Building windows amd64 ==="
 CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC="x86_64-w64-mingw32-gcc" go build -trimpath -o ./dist/pdf-annots2json.exe .
 cd ./dist
-zip ./pdf-annots2json.windows.amd64.zip ./pdf-annots2json.exe
+zip ./pdf-annots2json.windows.x64.zip ./pdf-annots2json.exe
 rm ./pdf-annots2json.exe
 cd ..
