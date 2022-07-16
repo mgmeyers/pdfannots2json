134af6c54a4ea786fcd7e64bd66977c39dd95903
mgmeyers <matthew.gregory.meyers@gmail.com>
2022-07-16 13:27:33 -0700

    Bump

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-const version = "v1.0.2"
+const version = "v1.0.4"
 
 var args struct {
 	Version      kong.VersionFlag `short:"v" help:"Display the current version of pdfannots2json"`
