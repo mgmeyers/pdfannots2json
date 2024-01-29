b117e79a9ca0979df4b92ebee901fbb3e4aceefc
0nyr <rascoussier.florian@gmail.com>
2024-01-29 19:42:57 +0100

    Added a base flake with dev shell and building of package.

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "pdfannots2json";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pdfannots2json = pkgs.buildGoModule {
+          pname = "pdfannots2json";
+          version = "0.1.0";  # Replace with the actual version
+
+          # Pointing src to the local directory of your project
+          src = ./.;  # Replace with the actual path to your local fork
+
+          # Since we are using a local source, vendorSha256 may not be necessary.
+          # If buildGoModule requires it, set it to a dummy value and adjust according to the error message.
+          vendorHash = null;
+          
+          # Customize build steps if necessary
+          buildPhase = ''
+            runHook preBuild
+            go build -o $out/bin/pdfannots2json
+            runHook postBuild
+          '';
+          
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/mgmeyers/pdfannots2json";
+            license = licenses.mit;
+            description = "A tool to convert PDF annotations to JSON";
+          };
+        };
+      in
+      {
+        packages.pdfannots2json = pdfannots2json;
+        defaultPackage = pdfannots2json;
+
+        # development environment
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            # Go
+            go
+          ];
+
+          buildInputs = with pkgs; [
+            pdfannots2json
+          ];
+        };
+      }
+    );
+}
